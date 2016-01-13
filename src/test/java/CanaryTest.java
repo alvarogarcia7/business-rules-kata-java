bd1a3a3546ca6b8fc0dbafa74f5bc22b70466c6b
Pau Muñoz+Alvaro Garcia <pau.business+derecerca@gmail.com>
2016-01-13 13:30:18 +0100

    initial commit: project, dependencies, canary test

---
+++ src/test/java/CanaryTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CanaryTest {
+
+	@Test
+	public void canary1 () {
+
+		assertThat(true, is(true));
+	}
+
+
+}
