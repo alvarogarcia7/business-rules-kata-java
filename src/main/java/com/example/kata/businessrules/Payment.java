adc961a66870841c3a0027b4b47f299b5b43e460
Pau Muñoz+Alvaro Garcia <pau.business+derecerca@gmail.com>
2016-01-13 14:32:48 +0100

    refactor: remove dead code, inline method

--- src/main/java/com/example/kata/businessrules/Payment.java
+++ src/main/java/com/example/kata/businessrules/Payment.java
@@ -1,15 +1,12 @@
 package com.example.kata.businessrules;
 
 public class Payment {
-	public Slip payPhysicalProduct () {
-		return new Slip();
-	}
 
 	public DuplicateSlip pay (final Book book) {
 		return new DuplicateSlip();
 	}
 
 	public Slip pay (final PhysicalProduct physicalProduct) {
-		return payPhysicalProduct();
+		return new Slip();
 	}
 }
