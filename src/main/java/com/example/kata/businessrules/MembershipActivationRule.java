2814a3ce3ef7e1d60b5cddbf163d750b89f24259
Alvaro Garcia <derecerca@gmail.com>
2016-01-19 21:03:52 +0100

    red: applying a MembershipActivationRule should activate the associated membership

---
+++ src/main/java/com/example/kata/businessrules/MembershipActivationRule.java
@@ -0,0 +1,9 @@
+package com.example.kata.businessrules;
+
+public class MembershipActivationRule {
+	public MembershipActivationRule (final Membership membership, final Payment payment) {
+	}
+
+	public void apply () {
+	}
+}
---
+++ src/test/java/com/example/kata/businessrules/RulesShould.java
@@ -0,0 +1,19 @@
+package com.example.kata.businessrules;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RulesShould {
+
+	@Test
+	public void activate_a_membership () {
+		Membership membership = new Membership();
+
+		MembershipActivationRule rule = new MembershipActivationRule(membership, new Payment());
+		rule.apply();
+
+		assertThat(membership.isActive(), is(true));
+	}
+}
