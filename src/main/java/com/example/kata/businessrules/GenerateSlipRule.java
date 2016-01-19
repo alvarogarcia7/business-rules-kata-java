60156e4642f200d8b20264f905bb4bdc73a35fe2
Alvaro Garcia <derecerca@gmail.com>
2016-01-19 22:26:48 +0100

    green

--- src/main/java/com/example/kata/businessrules/GenerateSlipRule.java
+++ src/main/java/com/example/kata/businessrules/GenerateSlipRule.java
@@ -1,14 +1,20 @@
 package com.example.kata.businessrules;
 
 public class GenerateSlipRule {
+	private final PhysicalProduct physicalProduct;
+	private final Payment payment;
+	private Slip result;
+
 	public GenerateSlipRule (final PhysicalProduct physicalProduct, final Payment payment) {
+		this.physicalProduct = physicalProduct;
+		this.payment = payment;
 	}
 
 	public void apply () {
-
+		result = new Slip();
 	}
 
 	public Slip result () {
-		return null;
+		return result;
 	}
 }
