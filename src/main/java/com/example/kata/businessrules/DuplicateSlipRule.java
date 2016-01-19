611e2c1e82c5ab151fa5dbd96ecb6e9ad747a820
Alvaro Garcia <derecerca@gmail.com>
2016-01-19 22:19:32 +0100

    red: generate duplicate slip

---
+++ src/main/java/com/example/kata/businessrules/DuplicateSlipRule.java
@@ -0,0 +1,14 @@
+package com.example.kata.businessrules;
+
+public class DuplicateSlipRule {
+	public DuplicateSlipRule (final Book book, final Payment payment) {
+
+	}
+
+	public void apply () {
+	}
+
+	public Void result () {
+		return null;
+	}
+}
--- src/test/java/com/example/kata/businessrules/RulesShould.java
+++ src/test/java/com/example/kata/businessrules/RulesShould.java
@@ -3,6 +3,8 @@ package com.example.kata.businessrules;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class RulesShould {
@@ -15,5 +17,15 @@ public class RulesShould {
 		rule.apply();
 
 		assertThat(membership.isActive(), is(true));
+	}
+
+	@Test
+	public void generate_duplicate_slip(){
+		Book book = new Book();
+		DuplicateSlipRule rule = new DuplicateSlipRule(book, new Payment());
+
+		rule.apply();
+
+		assertThat(rule.result(), is(not(nullValue())));
 	}
 }
