46e3539040745b25f8f7f4f75e191b912178035c
Alvaro Garcia <derecerca@gmail.com>
2016-01-19 21:04:18 +0100

    green

--- src/main/java/com/example/kata/businessrules/MembershipActivationRule.java
+++ src/main/java/com/example/kata/businessrules/MembershipActivationRule.java
@@ -1,9 +1,15 @@
 package com.example.kata.businessrules;
 
 public class MembershipActivationRule {
+	private final Membership membership;
+	private final Payment payment;
+
 	public MembershipActivationRule (final Membership membership, final Payment payment) {
+		this.membership = membership;
+		this.payment = payment;
 	}
 
 	public void apply () {
+		membership.activate();
 	}
 }
